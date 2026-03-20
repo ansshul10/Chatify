e9fa1ad9008c2b2c5b3ea51b6e3f0209a4747ed6
ansshul10 <ansshul10@gmail.com>
2026-03-20 17:30:04 +0530

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",  // http://localhost:5000/api
-  withCredentials: true,                           // cookie always send
+  baseURL: import.meta.env.VITE_API_URL + "/api",  
+  withCredentials: true,                           
   headers: {
     "Content-Type": "application/json",
   },
